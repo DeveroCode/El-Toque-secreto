0b7e80fd6957886f30dad3759e614fdb1ca2807d
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-04-16 22:03:47 -0500

    New: Bugs fixed and we create the foreach to cycle the foods

--- Assets/js/header.js
+++ Assets/js/header.js
@@ -1,12 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    scrollHeader();
-    scrollNav();
+document.addEventListener('DOMContentLoaded', function () {
+    startApp();
 });
 
-function scrollNav(){
+function startApp(){
+    scrollHeader();
+    scrollNav();
+    foods();
+}
+
+function scrollNav() {
     const enlaces = document.querySelectorAll('.navegacion a');
     enlaces.forEach(enlace => {
-        enlace.addEventListener('click', function(e){
+        enlace.addEventListener('click', function (e) {
             e.preventDefault();
             const seccion = document.querySelector(e.target.attributes.href.value);
             seccion.scrollIntoView({
@@ -16,24 +21,24 @@ function scrollNav(){
     });
 }
 
-function scrollHeader(){
+function scrollHeader() {
     const header = document.querySelector('#header');
     const enlaces = document.querySelectorAll('.link');
     const logo = document.querySelector('.logo');
     const guest = document.querySelector('#guest-info');
-    
-    window.addEventListener('scroll', function(){
+
+    window.addEventListener('scroll', function () {
         // console.log(guest.getBoundingClientRect());
-        if(guest.getBoundingClientRect().bottom < 832){
-            header.classList.add('stick','fixed-top', 'w-100', 'shadow', 'bg-white', 'text-warning', 'py-4');
+        if (guest.getBoundingClientRect().bottom < 832) {
+            header.classList.add('stick', 'fixed-top', 'w-100', 'shadow', 'bg-white', 'text-warning', 'py-4');
             logo.classList.add('tag');
             logo.classList.remove('text-white');
             enlaces.forEach(enlace => {
                 enlace.classList.add('tag');
                 enlace.classList.remove('text-white');
             });
-        }else{
-            header.classList.remove('stick','fixed-top', 'w-100', 'shadow', 'bg-white', 'text-warning');
+        } else {
+            header.classList.remove('stick', 'fixed-top', 'w-100', 'shadow', 'bg-white', 'text-warning');
             logo.classList.remove('tag');
             logo.classList.add('text-white');
             enlaces.forEach(enlace => {
@@ -41,7 +46,47 @@ function scrollHeader(){
                 enlace.classList.add('text-white');
             });
         }
-    });  
+    });
 }
 
+function obtenerDatosFoods(){
+    return fetch('../../Assets/js/food.json')
+    .then(response => response.json())
+    .then(data => {
+        // Aquí puedes acceder a los datos del archivo JSON
+        return Promise.resolve(data);
+    });
+}
 
+function foods(){
+    obtenerDatosFoods().then(data => {
+        data.comidas.forEach(food => {
+            if(food.nombre === 'Pizza Italiana' || food.nombre === 'Ensalada griega' || food.nombre === 'Ensalada de mariscos' || food.nombre === 'Ensalada de salmon'){
+                const father = document.querySelector('.food-images');
+                const div = document.createElement('div');
+                const img = document.createElement('img');
+                const p = document.createElement('p');
+                const a = document.createElement('a');
+
+                p.textContent = food.nombre;
+                img.src = food.imagen;
+                img.alt = 'Imagen de' + ' ' +food.nombre;
+                a.href = '#';
+                a.textContent = 'Ordena ahora';
+                console.log(food.nombre);
+
+                div.classList.add('images', 'text-center', 'mt-6');
+                p.classList.add('mt-2', 'fs-2', 'fw-bold');
+                a.classList.add('mb-2', 'fs-4', 'text-uppercase', 'text-primary');
+
+                
+                div.appendChild(img);
+                div.appendChild(p);
+                div.appendChild(a);
+                father.appendChild(div);
+            }
+        });
+    });
+}
+    
+  
