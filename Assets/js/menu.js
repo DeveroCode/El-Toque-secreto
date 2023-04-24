6d6cc4f8d627c8a09f5d948c57fd1698e2abed7c
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-04-23 20:26:18 -0500

    Update: function get foods finished

--- Assets/js/menu.js
+++ Assets/js/menu.js
@@ -11,6 +11,22 @@ function getAllFoods() {
     const arrayFood = Object.values(data.comidas);
 
     arrayFood.forEach(element => {
-        console.log(element);
+        const main = document.querySelector('.container-cards');
+
+        const card = document.createRange().createContextualFragment(`
+            <div class="mb-4 mb-lg-0 shadow col-md-3">
+                <div class="card rounded shadow-sm border-0">
+                    <img src="${element.imagen}" alt="imagen de ${element.nombre}" class="img-fluid img-size">
+                    <div class="card-body p-4 text-center">
+                        <h5 class="mb-2 fs-4 text-uppercase text-primary">
+                            <a href="#" class="text-dark text-decoration-none">${element.nombre}</a>
+                        </h5>
+                        <p class="small text-muted font-italic">$${element.precio}</p>
+                    </div>
+                </div>
+            </div>
+        `);
+
+        main.append(card);
     });
 }
